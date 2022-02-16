6f284b61c1441783358bfeee8f3af6f979ad5965
Kristou <kristiyan.svetoslavov.petkov@gmail.com>
2022-02-16 13:17:17 +0200

    added persistance with redux

--- client/src/index.tsx
+++ client/src/index.tsx
@@ -3,14 +3,17 @@ import ReactDOM from "react-dom";
 import App from "./App";
 import { BrowserRouter } from "react-router-dom";
 import { Provider } from "react-redux";
-import store from "./redux/store";
+import { store, persistor } from "./redux/store";
+import { PersistGate } from "redux-persist/es/integration/react";
 
 ReactDOM.render(
   <React.StrictMode>
     <Provider store={store}>
-      <BrowserRouter>
-        <App />
-      </BrowserRouter>
+      <PersistGate loading={null} persistor={persistor}>
+        <BrowserRouter>
+          <App />
+        </BrowserRouter>
+      </PersistGate>
     </Provider>
   </React.StrictMode>,
   document.getElementById("root")
