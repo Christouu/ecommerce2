cc62cad11e5d6dae89fabfe39fb7867c8ebe8d1e
Kristou <kristiyan.svetoslavov.petkov@gmail.com>
2022-02-16 14:03:34 +0200

    now you can register

--- client/src/pages/Register/Register.tsx
+++ client/src/pages/Register/Register.tsx
@@ -1,3 +1,5 @@
+import { useState } from "react";
+import axios from "axios";
 import {
   Aggrement,
   Button,
@@ -6,9 +8,29 @@ import {
   Input,
   Title,
   Wrapper,
+  Error,
 } from "./Register.styles";
+import { useNavigate } from "react-router-dom";
 
 const Register: React.FC = () => {
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+  const [repeatPass, setRepeatPass] = useState("");
+  const [email, setEmail] = useState("");
+  const history = useNavigate();
+
+  const handleClick = async (e: any) => {
+    e.preventDefault();
+
+    try {
+      await axios.post("http://localhost:5000/api/auth/register", {
+        email,
+        username,
+        password,
+      });
+      history("/login");
+    } catch (err) {}
+  };
   return (
     <Container>
       <Wrapper>
@@ -16,15 +38,30 @@ const Register: React.FC = () => {
         <Form>
           <Input placeholder="First Name" />
           <Input placeholder="Last Name" />
-          <Input placeholder="Username" />
-          <Input placeholder="Email" />
-          <Input placeholder="Password" />
-          <Input placeholder="Confirm Password" />
+          <Input
+            placeholder="Username"
+            onChange={(e) => setUsername(e.target.value)}
+          />
+          <Input
+            placeholder="Email"
+            onChange={(e) => setEmail(e.target.value)}
+          />
+          <Input
+            placeholder="Password"
+            onChange={(e) => setPassword(e.target.value)}
+            type="password"
+          />
+          <Input
+            placeholder="Confirm Password"
+            onChange={(e) => setRepeatPass(e.target.value)}
+            type="password"
+          />
           <Aggrement>
             By creating an account , I consent to the processing of my personal
             data in accordance with the <b>PRIVACY POLICY</b>
           </Aggrement>
-          <Button>CREATE</Button>
+          {password !== repeatPass && <Error>Password must match</Error>}
+          <Button onClick={handleClick}>CREATE</Button>
         </Form>
       </Wrapper>
     </Container>
